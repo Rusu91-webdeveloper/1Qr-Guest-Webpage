4097f222b01e8253c487fb70cc014066aca5995e
emanuel rusu <emanuelrusu@MacBookAir.fritz.box>
2024-12-13 12:57:53 +0100

    update dynamic and models

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,12 +1,14 @@
+import { Metadata } from "next";
 import connectDB from "@/config/database";
 import Machine from "@/models/equipment";
 import { notFound } from "next/navigation";
 
-export default async function MachinePage({
-  params,
-}: {
+type Props = {
   params: { id: string };
-}) {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function MachinePage({ params }: Props) {
   try {
     await connectDB();
     const data = await Machine.findById(params.id);
@@ -25,4 +27,11 @@ export default async function MachinePage({
     console.error("Error fetching machine:", error);
     return <div>Error: Unable to fetch machine details</div>;
   }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // This function is optional but recommended for SEO
+  return {
+    title: `Machine ${params.id}`,
+  };
 }
